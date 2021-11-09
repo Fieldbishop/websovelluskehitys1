413dcb66e00a2f86016858c1b02d458e7cc6debd
Fieldbishop <74423885+Fieldbishop@users.noreply.github.com>
2021-11-09 19:48:28 +0200

    7 ok

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -38,9 +38,48 @@ function addTask() {
     
 } // End of addTask() function.
 
+function removeDuplicates(){
+    'use strict';
+
+    // Reference to where the output goes:
+    let output = document.getElementById('output');
+
+    let message = '';
+
+    // temp array to fill with non duplicate values
+    let temp = [];
+    // flag so dont have to iterate over the whole array if duplicate has been spotted
+    let seen = false;
+
+    for (let i = 0; i < tasks.length; i++) {
+        seen=false;
+        for (let j = 0; j < temp.length; j++) {
+            if(temp[j]===tasks[i]){
+                seen = true;
+                break;
+            }
+        }
+        if(seen === false){
+            temp.push(tasks[i]);
+        }
+    }
+    tasks = temp;
+
+    //update list
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+
+    return false;
+}
+
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('remove').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;
